f7bebfb27f22fecade345349a3c65ed1e20fd3e9
Brian Zheng <brian.zheng@titansoft.com.sg>
2019-05-04 09:41:29 +0800

    implement by one method

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,45 @@
-﻿namespace DependencyInjectionWorkshop.Models
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Dapper;
+
+namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            var sourcePasswordHash = hash.ToString();
+
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                var passwordHash = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+                if (sourcePasswordHash != passwordHash)
+                {
+                    return false;
+                }
+            }
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var otpResult = response.Content.ReadAsAsync<string>().Result;
+                return otpResult == otp;
+            }
+
+            throw new Exception($"web api error, accountId:{accountId}");
+        }
     }
 }
