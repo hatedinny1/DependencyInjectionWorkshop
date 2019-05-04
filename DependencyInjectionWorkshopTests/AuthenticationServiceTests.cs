ab9e08473d9007993a1fbe7716a9321f8fa81b07
Brian Zheng <brian.zheng@titansoft.com.sg>
2019-05-04 16:03:21 +0800

    log account failed count when invalid

--- DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
+++ DependencyInjectionWorkshopTests/AuthenticationServiceTests.cs
@@ -11,27 +11,22 @@ namespace DependencyInjectionWorkshopTests
     public class AuthenticationServiceTests
     {
         private const string DefaultAccountId = "joey";
+        private const int DefaultFailedCount = 91;
         private const string DefaultHashPassword = "my hash password";
         private const string DefaultOtp = "123456";
         private const string DefaultPassword = "pw";
-        private IProfile _profile;
-        private IOtp _otpService;
-        private INotification _slackAdapter;
-        private IFailCounter _failCounter;
-        private IHash _sha256Adapter;
-        private ILogger _nLogAdapter;
         private AuthenticationService _authenticationService;
-
-        [SetUp]
-        public void SetUp()
+        private IFailCounter _failCounter;
+        private ILogger _logger;
+        private INotification _notification;
+        private IOtp _otpService;
+        private IProfile _profile;
+        private IHash _sha256Adapter;
+        [Test]
+        public void is_invalid_when_wrong_otp()
         {
-            _profile = Substitute.For<IProfile>();
-            _otpService = Substitute.For<IOtp>();
-            _slackAdapter = Substitute.For<INotification>();
-            _failCounter = Substitute.For<IFailCounter>();
-            _sha256Adapter = Substitute.For<IHash>();
-            _nLogAdapter = Substitute.For<ILogger>();
-            _authenticationService = new AuthenticationService(_profile, _sha256Adapter, _otpService, _nLogAdapter, _failCounter, _slackAdapter);
+            var isValid = WhenInvalid();
+            ShouldBeInvalid(isValid);
         }
 
         [Test]
@@ -41,13 +36,47 @@ namespace DependencyInjectionWorkshopTests
             GivenPassword(DefaultAccountId, DefaultHashPassword);
             GivenHash(DefaultPassword, DefaultHashPassword);
 
-            var isValid = _authenticationService.Verify(DefaultAccountId, DefaultPassword, DefaultOtp);
-            Assert.IsTrue(isValid);
+            var isValid1 = _authenticationService.Verify(DefaultAccountId, DefaultPassword, DefaultOtp);
+            var isValid = isValid1;
+            ShouldBeValid(isValid);
         }
 
-        private void GivenOtp(string accountId, string Otp)
+        [Test]
+        public void log_account_failed_count_when_invalid()
         {
-            _otpService.GetCurrentOtp(accountId).ReturnsForAnyArgs(Otp);
+            _failCounter.Get(DefaultAccountId).ReturnsForAnyArgs(DefaultFailedCount);
+
+            WhenInvalid();
+
+            LogShouldContain(DefaultAccountId, DefaultFailedCount.ToString());
+        }
+
+        [Test]
+        public void notify_user_when_invalid()
+        {
+            WhenInvalid();
+            ShouldNotifyUser();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _profile = Substitute.For<IProfile>();
+            _otpService = Substitute.For<IOtp>();
+            _notification = Substitute.For<INotification>();
+            _failCounter = Substitute.For<IFailCounter>();
+            _sha256Adapter = Substitute.For<IHash>();
+            _logger = Substitute.For<ILogger>();
+            _authenticationService = new AuthenticationService(_profile, _sha256Adapter, _otpService, _logger,
+                _failCounter, _notification);
+        }
+        private static void ShouldBeInvalid(bool isValid)
+        {
+            Assert.IsFalse(isValid);
+        }
+        private static void ShouldBeValid(bool isValid)
+        {
+            Assert.IsTrue(isValid);
         }
 
         private void GivenHash(string password, string hashPassword)
@@ -55,9 +84,33 @@ namespace DependencyInjectionWorkshopTests
             _sha256Adapter.GetHash(password).ReturnsForAnyArgs(hashPassword);
         }
 
+        private void GivenOtp(string accountId, string Otp)
+        {
+            _otpService.GetCurrentOtp(accountId).ReturnsForAnyArgs(Otp);
+        }
+
         private void GivenPassword(string accountId, string password)
         {
             _profile.GetPassword(accountId).ReturnsForAnyArgs(password);
+        }
+
+        private void LogShouldContain(string accountId, string failedCount)
+        {
+            _logger.Received(1).Info(Arg.Is<string>(m => m.Contains(accountId) && m.Contains(failedCount)));
+        }
+
+        private void ShouldNotifyUser()
+        {
+            _notification.Received(1).PushMessage(Arg.Any<string>());
+        }
+
+        private bool WhenInvalid()
+        {
+            GivenOtp(DefaultAccountId, DefaultOtp);
+            GivenPassword(DefaultAccountId, DefaultHashPassword);
+            GivenHash(DefaultPassword, DefaultHashPassword);
+
+            return _authenticationService.Verify(DefaultAccountId, DefaultPassword, "wrong otp");
         }
     }
 }
